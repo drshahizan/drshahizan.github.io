16e8416831bbfee8f8cf3900f1b4f762c3f26a09
Mohd Shahizan Othman <shahizan@utm.my>
2026-09-22 23:29:32 +0800

    update

---
+++ slr/assets/config.js
@@ -0,0 +1,6 @@
+window.SLR_CONFIG = {
+  // Paste the deployed Google Apps Script Web App URL below.
+  // Leave blank to use browser-based demo storage.
+  apiUrl: "https://script.google.com/a/macros/utm.my/s/AKfycbxeTpqvnP47FjZgnohPuHwkNHjGD4W2HNbf9xL_5JcEt1hjY-fS6Rhbc1leDT_JVRg/exec",
+  storageKey: "utm_slr_phase1_articles"
+};
